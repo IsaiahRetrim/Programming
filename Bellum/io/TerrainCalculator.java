9aaf8ff451dfc33aacd22631046b9f3cec5a8918
NickAnson <nick.anson00@gmail.com>
2016-11-16 21:16:38 -0600

    f
    Got em

--- Bellum/io/TerrainCalculator.java
+++ Bellum/io/TerrainCalculator.java
@@ -5,6 +5,23 @@ package Programming.Bellum.io;
  */
 public class TerrainCalculator {
     public static void main(String[] args) {
-        
+
+    }
+}
+class chancesOfAnyTerrain {
+    public double chancePercent() {
+        public boolean basesWithin500K(){
+            
+        }
+        public boolean basesWithin750K() {
+
+        }
+        public boolean basesWithin1000k() {
+
+        }
+        public boolean basesMoreThan1000K() {
+
+        }
+
     }
 }
