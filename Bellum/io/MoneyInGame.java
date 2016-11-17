b8a6f220f819776f5cf0db132601d1d2ca14b866
NickAnson <nick.anson00@gmail.com>
2016-11-16 22:22:24 -0600

    Holy crap long
    this is how we do it!

---
+++ Bellum/io/MoneyInGame.java
@@ -0,0 +1,7 @@
+package Programming.Bellum.io;
+
+/**
+ * Created by Nick on 11/16/2016.
+ */
+public class MoneyInGame {
+}
--- Bellum/io/TerrainCalculator.java
+++ Bellum/io/TerrainCalculator.java
@@ -4,23 +4,48 @@ package Programming.Bellum.io;
  * Created by Nick on 11/16/2016.
  */
 public class TerrainCalculator {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        chanceCalculator cc = new chanceCalculator();
+        double x = cc.Bases500k();
+        System.out.println("You have: " + (int)x +"  bases that cost 500k around you");
     }
+
 }
 class Calculate {
-
         public  boolean basesWithin500K() {
             return true;
+
         }
         public boolean basesWithin750K() {
+            return true;
 
         }
         public boolean basesWithin1000k() {
+            return true;
 
         }
         public boolean basesMoreThan1000K() {
-
+            return true;
         }
     }
+class chanceCalculator {
+    public int PERCENT = 100;
+    double basesBeforeRunOut;
+    public double  Bases500k() {
+        int chance500k = (int)(Math.random() * 10 + 9);
 
+        for(double i = 0;i < PERCENT;++i) {
+            double bases = ((chance500k / i) * 100);
+            this.basesBeforeRunOut = bases;
+        }
+        final double BASESBEFORERUNOUT = basesBeforeRunOut/10;
+        return BASESBEFORERUNOUT ;
+    }
+    public float Bases750k() {
+        return 0;
+    }
+    public float Baese1000k() {
+        return 0;
+    }
+}
