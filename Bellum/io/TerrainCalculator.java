72fd5c64dfd4607a33cb1048da8245057adcd006
NickAnson <nick.anson00@gmail.com>
2016-11-16 23:29:53 -0600

    sdf
    sdf

--- Bellum/io/TerrainCalculator.java
+++ Bellum/io/TerrainCalculator.java
@@ -36,13 +36,12 @@ class chanceCalculator {
         int chance500k = (int)(Math.random() * 10 + 9);
 
         for(double i = 0;i < PERCENT;++i) {
-            double bases = ((chance500k / i) * 100);
-            this.basesBeforeRunOut = bases;
+            this.basesBeforeRunOut = ((chance500k / i) * 100);
         }
-        final double BASESBEFORERUNOUT = basesBeforeRunOut/10;
-        return BASESBEFORERUNOUT ;
+        return basesBeforeRunOut ;
     }
     public float Bases750k() {
+        
         return 0;
     }
     public float Baese1000k() {
